4d1df408c9bd56db87cb418a9a2ff3c29e58bab0
Victor Perin <me@victorperin.com>
2016-02-17 22:06:42 -0200

    inserting first version

---
+++ index.js
@@ -0,0 +1,30 @@
+var colors = require('colors');
+var util = require('util');
+var exec = require('child_process').exec;
+
+function random (low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+function writeText(text, timing){
+    exec("toilet -f bigmono12 -w 200 "+text, function(error, stdout, stderr){
+      process.stdout.write('\033c');
+
+      process.stdout.write(stdout.toString().random + '\r');
+    });
+}
+
+var x = true;
+var y = 0;
+
+setInterval(function(){
+  if(x){
+    writeText(process.argv[2]+" o/",y);
+    x = false;
+  }
+  else{
+    writeText(process.argv[2]+" o_", y);
+    x = true;
+
+  }
+},50);
